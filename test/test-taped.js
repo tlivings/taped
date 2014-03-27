dfd15401f57e49a9b5270a917a779e25efbbf8c8
Trevor <tlivings@gmail.com>
2014-03-26 21:37:26 -0500

    Wrapping callbacks instead of using prerun event.

--- test/test-taped.js
+++ test/test-taped.js
@@ -17,16 +17,21 @@ test('test', {
 
     t.test('plan1', {
         before: function (t, next) {
-            t.equal(count++, 1, 'beforeEach: count is 1.'); //Counts against tests's plan.
+            t.equal(count++, 1, 'plan1.before: count is 1.'); //Counts against tests's plan.
             next();
         },
         after: function () {
             count--;
-            t.pass('called afterEach'); //Count's against suite's plan.
+            t.pass('called plan1.after'); //Count's against suite's plan.
         }
     }, function (t) {
         t.plan(2); //beforeEach, and this test.
-        t.equal(count, 2, 'called before and beforeEach.');
+        t.equal(count, 2, 'called before and plan1.before.');
+    });
+
+    t.test('plan2', function (t) {
+        t.plan(1);
+        t.pass('ran without hooks.');
     });
 
 });
